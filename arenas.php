cd96c8ecf2f42f0c2adb6c25fdc911b2f7ddbe17
jmanosu <jmanosu@gmail.com>
2018-06-06 13:16:25 -0700

    added funcionality for champions page and arenas page

--- arenas.php
+++ arenas.php
@@ -17,77 +17,109 @@
 
 <body>
   <?php
-	function display($sqltable){
-		echo "<table id='t01' border='t'><tr>";
-		$fields_num = mysqli_num_fields($sqltable);
-		for($i = 0;$i < $fields_num; $i++){
-			$field = mysqli_fetch_field($sqltable);
-			echo "<td><b>$field->name</b></td>";
-		}
-		echo "</tr>\n";
-		while($row = mysqli_fetch_row($sqltable)) {
-			echo "<tr>";
-			foreach($row as $cell){
-				echo "<td>$cell</td>";
-			}
-			echo "</tr>\n";
-		}
-	}
-
-
 	$conn = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
 	if (!$conn) {
 		die('Could not connect: ' . mysql_error());
 	}
+
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		foreach($_POST as $key => $value) {
+			if(isset($_GET['arena'])){
+				$arena = $_GET['arena'];
+				if($value == "add"){
+					$queryOut = "UPDATE Champions C SET C.arena = '$arena' WHERE C.cID = '$key' ";
+					$result = mysqli_query($conn, $queryOut);
+					if(!$result){
+						echo $queryOut;
+						die("Query couldn't complete");
+					}
+				}
+			}
+		}
+	}
+
+
+	function display($sqltable){
+		$return = "<table id='t01' border='t'><tr>";
+		$fields_num = mysqli_num_fields($sqltable);
+		for($i = 0;$i < $fields_num; $i++){
+			$field = mysqli_fetch_field($sqltable);
+			$return .=  "<td><b>$field->name</b></td>";
+		}
+		$return .= "</tr>\n";
+		while($row = mysqli_fetch_row($sqltable)) {
+			$return .= "<tr>";
+			foreach($row as $cell){
+				$return .= "<td>$cell</td>";
+			}
+			$return .= "</tr>\n";
+		}
+		$return .= "</table>";
+		return $return;
+	}
+
+
+
 	if (checkAuth(true) != "") {
 	if(!isset($_GET['arena'])){
 				$arenas = mysqli_query($conn,"SELECT * FROM Arena");
 				echo "<div class=arena-container>";
-				if(mysqli_num_rows($arenas) > 0 ){
-					while($row = mysqli_fetch_row($arenas)){
+				while($row = mysqli_fetch_row($arenas)){
 						echo "<div class='arena'>";
 						echo "<div class='arena-contents' id='name'>".$row[0]."</div>";
 						echo "<div class='arena-contents' id='weather'> <bold>Weather:</bold> ".$row[1]."</div>";
-						echo "<div class='arena-contents' id='cNum'> <bol>Champions:</bold> ".$row[2]."</div>";
+						echo "<div class='arena-contents' id='cNum'> <bold>Champions:</bold> ".$row[2]."</div>";
 						echo "<input class='arena-contents' id='aview-button' type='button' onclick=\"window.location.href='arenas.php?arena=".$row[0]."'\" name='$row[0]' value='View'/>";
 						echo "</div>";
-					}
 				}
 				echo "</div>";
 				mysqli_free_result($arenas);
 	} else {
 		if(checkAuth(false) != ""){
 			$username = $_SESSION['username'];
-			$queryIn = "SELECT C.cID, C.name, C.level, C.exp, C.power, C.intelligence, C.endurance FROM Champions C WHERE C.username = '$username' AND C.arena IS NULL AND C.alive = 1";
+			$queryIn = "SELECT C.cID, C.name, C.power, C.intelligence, C.endurance FROM Champions C WHERE C.username = '$username' AND C.arena IS NULL AND C.alive = 1";
 			$resultIn = mysqli_query($conn, $queryIn);
-			echo "<table id='t01' border='t'><tr>";
-			while($row = mysqli_fetch_row($resultIn)) {
-				echo "<tr>";
-				foreach($row as $cell){
-					echo "<td>$cell</td>";
-				}
-				echo "</tr>\n";
+			echo "<div class='add-champion'>";
+			while($row = mysqli_fetch_assoc($resultIn)){
+					echo "<div class='arena'>";
+					echo "<h4>".$row['name']."</h4>";
+					echo "<p><bold>P:</bold> ".$row['power']."<br>";
+					echo "<bold>I:</bold> ".$row['intelligence']."<br>";
+					echo "<bold>E:</bold> ".$row['endurance']."</p>";
+					echo "<form method='POST'>";
+					echo "<input id='button' type='submit' value='add' name=".$row['cID'].">";
+					echo "</form>";
+					echo "</div>";
 			}
+			echo "</div>";
+
+			$var = $_GET['arena'];
+			$query = "SELECT * FROM Events WHERE arena = '$var'";
+
+			$result = mysqli_query($conn, $query);
+			if(!$result){
+				die("Query couldn't complete");
+			}
+			$msg = "<div class='events'>";
+			$msg .= "<h1>Events</h1>";
+			$msg .= display($result);
+			mysqli_free_result($result);
+			$msg .= "</div>\n";
+			echo $msg;
+
+			$query = "SELECT C.cID, C.name, C.power, C.intelligence, C.endurance FROM Champions C WHERE C.arena = '$var'";
+
+			$result = mysqli_query($conn, $query);
+			if(!$result){
+				die("Query couldn't complete");
+			}
+			$msg = "<div class=champions>";
+			$msg .= "<h1>Champions</h1>";
+			$msg .= display($result);
+			mysqli_free_result($result);
+			$msg .= "</div>";
+			echo $msg;
 		}
-		$var = $_GET['arena'];
-		$query = "SELECT * FROM Events WHERE arena = '$var'";
-
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			die("Query couldn't complete");
-		}
-		echo "<h1>Events</h1>";
-		display($result);
-
-		$query = "SELECT * FROM Champions WHERE arena = '$var'";
-
-		$result = mysqli_query($conn, $query);
-		if(!$result){
-			die("Query couldn't complete");
-		}
-
-		echo "<h1>Champions</h1>";
-		display($result);
 	}
 
 		mysqli_close($conn);
--- mychampion.php
+++ mychampion.php
@@ -5,7 +5,7 @@
 		error_reporting(E_ALL);
 		$currentpage="My Champions";
 		include "pages.php";
-        include "header.php";
+    include "header.php";
 ?>
 <html>
 	<head>
@@ -16,8 +16,71 @@
 	if (checkAuth(true) != "") {
 ?>
 <body>
-  <?php echo 'champion list should display here'; ?>
- </body>
+	<?php
+		$msg = "Add a Champion!";
+	// change the value of $dbuser and $dbpass to your username and password
+		include 'connectvars.php';
+		$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+		if (!$conn) {
+			die('Could not connect: ' . mysql_error());
+		}
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+	// Escape user inputs for security
+			$username = $_SESSION['username'];
+			$name = mysqli_real_escape_string($conn, $_POST['name']);
+			$power = mysqli_real_escape_string($conn, $_POST['power']);
+			$intelligence = mysqli_real_escape_string($conn, $_POST['intelligence']);
+			$endurance = mysqli_real_escape_string($conn, $_POST['endurance']);
+			$queryIn = "SELECT * FROM Sponsors where username='$username' ";
+			$resultIn = mysqli_query($conn, $queryIn);
+	        // See if username is already in the table
+			if($username != ""){
+				$query = "INSERT INTO Champions (cID, name, username, power, intelligence, endurance) VALUES (100, '$name', '$username', '$power', '$intelligence', '$endurance')";
+				if(mysqli_query($conn, $query)){
+	      	echo '<p class="white">Champion Created</p>';
+				} else{
+				echo "ERROR: Could not able to execute $query. " . mysqli_error($conn);
+				}
+			}
+	}
+	// close connection
+	mysqli_close($conn);
 
+	?>
+		<section>
+	    <h2> <?php echo $msg; ?> </h2>
+
+	<form method="post" id="addForm">
+	<fieldset>
+		<legend>User Info:</legend>
+
+	    <p>
+	        <label for="">Name:</label>
+	        <input type="text" class="required" name="name" id="name">
+	    </p>
+
+			<p>
+					<label for="">Power:</label>
+					<input type="number" class="required" name="power" id="power">
+			</p>
+
+			<p>
+					<label for="">Intelligence:</label>
+					<input type="number" class="required" name="intelligence" id="intelligence">
+			</p>
+
+			<p>
+					<label for="">Endurance:</label>
+					<input type="number" class="required" name="endurance" id="endurance">
+			</p>
+
+	</fieldset>
+
+	      <p>
+	        <input type = "submit"  value = "Submit" />
+	        <input type = "reset"  value = "Clear Form" />
+	      </p>
+	</form>
+ </body>
 </html>
 <?php } ?>
